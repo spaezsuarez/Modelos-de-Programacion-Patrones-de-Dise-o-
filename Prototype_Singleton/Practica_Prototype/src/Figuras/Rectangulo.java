4d1c7f7f18151e5973524ddc051550aed4e76868
PykyE <miguel35697@hotmail.com>
2020-10-24 16:42:59 -0500

    Ajustes lógica

--- Prototype_Singleton/Practica_Prototype/src/Figuras/Rectangulo.java
+++ Prototype_Singleton/Practica_Prototype/src/Figuras/Rectangulo.java
@@ -1,64 +1,63 @@
 package Figuras;
 
-/**
- *
- * @author jorge malaver
- */
 import Interface.Figuras_geometricas;
 
-public class Rectangulo implements Figuras_geometricas{
-    
+public class Rectangulo extends Figuras_geometricas {
+
     private static Rectangulo instancia;
-    
-    private String Nombre;
-    private int pos_x, pos_y;
-    private double area;
-    
-    private Rectangulo(){}
-    
-    public static Rectangulo getInstance(){
-        if(instancia == null){
+
+    private int ancho, altura;
+
+    private Rectangulo() {
+    }
+
+    public static Rectangulo getInstance() {
+        if (instancia == null) {
             instancia = new Rectangulo();
         }
-        
         return instancia;
-        
     }
-    
+
     @Override
-    public void setNombre(String nombre){
-     this.Nombre = nombre;   
+    public void setNombre(String nombre) {
+        this.Nombre = nombre;
     }
+
     @Override
-    public String getNombre(){
+    public String getNombre() {
         return Nombre;
     }
-     
-    @Override
-    public void tamaño(int x, int y){
-           pos_x = x;
-           pos_y = y;
-        
+
+    public int getAncho() {
+        return ancho;
     }
-    
-    @Override
-    public void getposicion(){
-       System.out.println("Altura del rectangulo .." + this.pos_y);
-       System.out.println("Ancho del rectangulo...." + this.pos_x);  
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
     }
-    
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
     @Override
-    public Figuras_geometricas clonar(){
-       
-        Figuras_geometricas geometrica= new Rectangulo();
+    public Rectangulo clonar() {
+        Rectangulo geometrica = new Rectangulo();
         geometrica.setNombre(this.Nombre);
-        geometrica.tamaño(this.pos_x, this.pos_y);
+        geometrica.posicion(this.pos_x, this.pos_y);
+        geometrica.setAncho(ancho);
+        geometrica.setAltura(altura);
+        geometrica.setArea();
+        geometrica.setArea(area);
         return geometrica;
     }
 
     @Override
-    public double getArea() {
-        area = (pos_x*pos_y)/2;
-        return area;
+    public void setArea() {
+        area = ancho * altura;
     }
 }
--- Prototype_Singleton/Practica_Prototype/src/Interface/Figuras_geometricas.java
+++ Prototype_Singleton/Practica_Prototype/src/Interface/Figuras_geometricas.java
@@ -1,23 +1,45 @@
 package Interface;
 
-/**
- *
- * @author jorge malaver
- */
-public interface Figuras_geometricas { //Interfaz referente al prototipo abstracto que delegara la funcion de clonarse
-    
-    
+public abstract class Figuras_geometricas { //Interfaz referente al prototipo abstracto que delegara la funcion de clonarse
+
     /*
         Metodos a que indican el comportamiento que deben tener las figuras que implementen 
         la interfaz Figuras_Geometricas
-    */
-    public void setNombre(String nombre);
-    public String getNombre();
-    public void tamaño(int x, int y);
-    public void getposicion();
-    public double getArea();
-   
-    
-    public Figuras_geometricas clonar(); //Metodo clave del patron, el cual devolvera una copia del objeto
-                                         // actual que implemente esta interfaz
+     */
+    protected String Nombre;
+    protected int pos_x, pos_y;
+    protected double area;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getPos_x() {
+        return pos_x;
+    }
+
+    public int getPos_y() {
+        return pos_y;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public abstract void setArea();
+
+    public void posicion(int x, int y) {
+        pos_x = x;
+        pos_y = y;
+    }
+
+    public abstract Figuras_geometricas clonar(); //Metodo clave del patron, el cual devolvera una copia del objeto actual que implemente esta interfaz
 }
