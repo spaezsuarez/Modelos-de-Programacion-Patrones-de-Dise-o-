8ee580475a947c7ce96172e4edbea7d45fde0792
PykyE <miguel35697@hotmail.com>
2020-11-19 12:03:34 -0500

    Última versión FlyWeight
    12:03 19/11/20

--- Flyweigth/PracticaFlyweight/src/gui/VentanaDestornillador.java
+++ Flyweigth/PracticaFlyweight/src/gui/VentanaDestornillador.java
@@ -7,7 +7,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
-import javax.swing.JPanel;
 import java.awt.event.*;
 
 import models.Tornillo;
@@ -15,79 +14,74 @@ import abstraction.Destornillador;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 
+public class VentanaDestornillador extends JFrame implements ActionListener {
 
-public class VentanaDestornillador extends JFrame implements ActionListener{
-    
-    private final int ANCHO,ALTO;
-    private final JButton btnDesatornillar,btnCrearTornillo;
+    private final int ANCHO, ALTO;
+    private final JButton btnDesatornillar;
     private final JLabel title;
     private final JComboBox inputUno;
     private JLabel panel;
-    
+
     private Tornillo tornillo;
     private Destornillador destornillador;
-    
-    public VentanaDestornillador(Destornillador destornillador){
+
+    public VentanaDestornillador(Destornillador destornillador) {
         ANCHO = 450;
         ALTO = 450;
         btnDesatornillar = new JButton("Desatornillar");
-        btnCrearTornillo = new JButton("Crear tornillo");
         inputUno = new JComboBox();
         title = new JLabel("Ingrese tipo de destornillador: ");
         panel = new JLabel();
-        
+
         this.destornillador = destornillador;
     }
-    
-    public void initElements(){
-        btnDesatornillar.setSize(new Dimension(150,35));
-        btnDesatornillar.setLocation(50, 350);
-        btnDesatornillar.setFont(new Font("Arial",Font.BOLD,15));
+
+    public void initElements() {
+        btnDesatornillar.setSize(new Dimension(150, 35));
+        btnDesatornillar.setLocation((this.getWidth() - btnDesatornillar.getWidth()) / 2, 350);
+        btnDesatornillar.setFont(new Font("Arial", Font.BOLD, 15));
         btnDesatornillar.setFocusable(false);
         btnDesatornillar.addActionListener(this);
         add(btnDesatornillar);
-        
-        btnCrearTornillo.setSize(new Dimension(150,35));
-        btnCrearTornillo.setLocation(250, 350);
-        btnCrearTornillo.setFont(new Font("Arial",Font.BOLD,15));
-        btnCrearTornillo.setFocusable(false);
-        btnCrearTornillo.addActionListener(this);
-        add(btnCrearTornillo);
-        
-        title.setSize(new Dimension(250,35));
-        title.setFont(new Font("Arial",Font.BOLD,15));
+
+        title.setSize(new Dimension(250, 35));
+        title.setFont(new Font("Arial", Font.BOLD, 15));
         title.setLocation(20, 35);
         add(title);
-        
-        panel.setSize(new Dimension(200,200));
-        panel.setLocation((this.getWidth() - panel.getWidth())/2, 100);
+
+        panel.setSize(new Dimension(200, 200));
+        panel.setLocation((this.getWidth() - panel.getWidth()) / 2, 100);
         add(panel);
-        
-        inputUno.setSize(new Dimension(150,30));
-        inputUno.setFont(new Font("Arial",Font.BOLD,15));
+
+        inputUno.addItemListener((ae) -> {
+            String nombre = String.valueOf(inputUno.getSelectedItem());
+            tornillo = new Tornillo(nombre);
+            setImage("estado_uno");
+        });
+
+        inputUno.setSize(new Dimension(150, 30));
+        inputUno.setFont(new Font("Arial", Font.BOLD, 15));
         inputUno.setLocation(250, 35);
         inputUno.addItem("Tornillo_uno");
         inputUno.addItem("Tornillo_dos");
         inputUno.addItem("Tornillo_tres");
         add(inputUno);
-        
-       
     }
-    
-    public void initTemplate(){
+
+    public void initTemplate() {
         setLayout(null);
         setTitle("Destornillador");
-        setSize(new Dimension(ANCHO,ALTO));
+        setSize(new Dimension(ANCHO, ALTO));
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setResizable(false);
         initElements();
         setLocationRelativeTo(null);
         setVisible(true);
     }
-    
-    public void setImage(String folder){
-        
-        String ruta = new String("src/resources/"+folder+"/"+tornillo.getNombre()+".png");
+
+    public void setImage(String folder) {
+
+        String ruta = new String("src/resources/" + folder + "/" + tornillo.getNombre() + ".png");
         ImageIcon icono = new ImageIcon(ruta);
         Image imagen = icono.getImage();
         Image nuevaImagen = imagen.getScaledInstance(panel.getWidth(), panel.getHeight(), Image.SCALE_SMOOTH);
@@ -98,20 +92,13 @@ public class VentanaDestornillador extends JFrame implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent event) {
-        if(event.getSource() == btnCrearTornillo){
-            String nombre = String.valueOf(inputUno.getSelectedItem());
-            tornillo = new Tornillo(nombre);
-            setImage("estado_uno");
-            
-        }else if(event.getSource() == btnDesatornillar){
+        if (event.getSource() == btnDesatornillar) {
             destornillador.desatornillar(tornillo);
-            
-            if(tornillo.getEstado()){
+            if (tornillo.getEstado()) {
                 setImage("estado_uno");
-            }else{
+            } else {
                 setImage("estado_dos");
             }
         }
     }
-    
 }
